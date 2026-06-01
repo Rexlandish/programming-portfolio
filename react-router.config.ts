fbb311ea1f7629d3c50808aff50b969e082c0a26
Kristian Baird <rexlandish@gmail.com>
2026-06-01 12:58:11 +0100

    Update react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+    // Config options...
+
+    basename: "/programming-portfolio",
+    // Server-side render by default, to enable SPA mode set this to `false`
+    ssr: true,
 } satisfies Config;
