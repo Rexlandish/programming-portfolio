69a71099985cf659022352ca145969c68e92eb26
Kristian Baird <rexlandish@gmail.com>
2026-06-01 13:09:07 +0100

    Added slash to basename

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/programming-portfolio",
+    base: "/programming-portfolio/",
     plugins: [tailwindcss(), reactRouter()],
     resolve: {
         tsconfigPaths: true,
